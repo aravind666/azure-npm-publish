9635c1b7edca3b9f0e1138bc509c9ea0fb6b9471
Aravind.H.U <aravind.udayashankara@gmail.com>
2022-05-09 16:24:29 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,12 +18,5 @@ fi
 echo "Here is the list of files"
 ls -la 
 
-perl -pi -e "s/EMAIL/$2/g" .npmrc
-perl -pi -w -e "s{ORGANIZATION}{$3}g" .npmrc
-perl -pi -w -e "s{FEED}{$4}g"  .npmrc
-perl -pi -e "s/ANY_VALUE_BUT_NOT_AN_EMPTY_STRING/$5/g"  .npmrc
-perl -pi -e "s/BASE64_ENCODED_PERSONAL_ACCESS_TOKEN/$6/g"  .npmrc
-
-
 ls -ltr
 npm publish --registry=$7
