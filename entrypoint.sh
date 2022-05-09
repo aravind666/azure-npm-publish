f7ea56a812267099bb31664bd0f8c5c4e2f1fdda
Aravind.H.U <aravind.udayashankara@gmail.com>
2022-05-09 15:48:25 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,11 +18,11 @@ fi
 echo "Here is the list of files"
 ls -la 
 
-perl -pi -e "s/EMAIL/$2/g" /.npmrc
-perl -pi -w -e "s{ORGANIZATION}{$3}g" /.npmrc
-perl -pi -w -e "s{FEED}{$4}g" /default.npmrc
-perl -pi -e "s/ANY_VALUE_BUT_NOT_AN_EMPTY_STRING/$5/g" /.npmrc
-perl -pi -e "s/BASE64_ENCODED_PERSONAL_ACCESS_TOKEN/$6/g" /.npmrc
+perl -pi -e "s/EMAIL/$2/g" .npmrc
+perl -pi -w -e "s{ORGANIZATION}{$3}g" .npmrc
+perl -pi -w -e "s{FEED}{$4}g"  .npmrc
+perl -pi -e "s/ANY_VALUE_BUT_NOT_AN_EMPTY_STRING/$5/g"  .npmrc
+perl -pi -e "s/BASE64_ENCODED_PERSONAL_ACCESS_TOKEN/$6/g"  .npmrc
 
 
 ls -ltr
